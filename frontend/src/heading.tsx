9a33d6d33fc5ddaa225716174c5dc939a7174648
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2025-11-03 22:01:44 -0500

    upload what I have

---
+++ frontend/src/heading.tsx
@@ -0,0 +1,5 @@
+export default function Heading() {
+    return (
+        <h1 className="text-3xl font-bold mb-6 text-center">Twinner Point Cloud Panel</h1>
+    )
+}
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,21 +1,36 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 import { createBrowserRouter, Link } from "react-router";
 import { RouterProvider } from "react-router/dom";
+import UploadForm from "./upload";
+import Heading from "./heading";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>
-      <p>Upload: <Link to="/upload">Upload Page</Link></p>
-      <p>View: <Link to="/view">View Page</Link></p>
-    </div>,
+    element: (
+      <div>
+        <p>
+          Upload: <Link to="/upload">Upload Page</Link>
+        </p>
+        <p>
+          View: <Link to="/view">View Page</Link>
+        </p>
+      </div>
+    ),
+  },
+  {
+    path: "/upload",
+    element: <UploadForm />,
   },
 ]);
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
-)
+    <div className="font-sans">
+      <Heading />
+      <RouterProvider router={router} />
+    </div>
+  </StrictMode>
+);
