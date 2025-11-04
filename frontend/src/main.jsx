1182bf6e84de194fd9b9a24a31d1c32d7d356fb3
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2025-11-03 21:30:25 -0500

    Add react router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { createBrowserRouter, Link } from "react-router";
+import { RouterProvider } from "react-router/dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>
+      Upload: <Link to="/upload">Upload Page</Link>
+      View: <Link to="/view">View Page</Link>
+    </div>,
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
