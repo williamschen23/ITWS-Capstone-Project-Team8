a00f04abfa826e7ed77dba08783add2c2db4fc5d
williamschen23 <chenwill2005@gmail.com>
2025-11-29 16:48:30 -0500

    update potree viewer

--- frontend/src/components/PotreeViewer.jsx
+++ frontend/src/components/PotreeViewer.jsx
@@ -1,7 +1,9 @@
+import { BASE_API_URL } from "../scripts/config.js";
+
 export default function PotreeViewer({ pointcloudName }) {
   return (
     <iframe
-      src={`http://127.0.0.1:8080/viewer/${pointcloudName}`}
+      src={`${BASE_API_URL}/viewer/${pointcloudName}`}
       style={{ width: '100%', height: '100%', border: 'none', display: 'block' }}
       title={`Potree Viewer - ${pointcloudName}`}
     />
