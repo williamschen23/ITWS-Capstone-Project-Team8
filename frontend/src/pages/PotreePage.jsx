9eac65fcd780299185fdc6576f8724181a14ad64
williamschen23 <chenwill2005@gmail.com>
2025-12-01 12:57:54 -0500

    update import

--- frontend/src/pages/PotreePage.jsx
+++ frontend/src/pages/PotreePage.jsx
@@ -3,7 +3,7 @@ import PotreeViewer from "@/components/PotreeViewer.jsx";
 import UploadModal from "@/components/UploadModal.jsx";
 import StatusBadge from "@/components/StatusBadge.jsx";
 import { BASE_API_URL } from "../scripts/config.js";
-import ToolTip from "@/components/Tooltip.jsx";
+import ToolTip from "@/components/ToolTip.jsx";
 
 export default function PotreePage() {
   const [pointClouds, setPointClouds] = useState({});
